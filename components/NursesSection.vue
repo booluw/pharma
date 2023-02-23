f8df8d043fc069ed3132b0b9665c765e4ca38082
= <=>
2023-02-23 09:00:18 +0100

    bugfix

--- components/NursesSection.vue
+++ components/NursesSection.vue
@@ -1,11 +1,11 @@
 <template>
-  <div class="bg-gray-100 text-black py-20 pl-20 md:p-40">
+  <div class="bg-gray-100 text-black py-20 px-10 md:p-40">
     <div class="w-2/3 md:w-1/2 relative">
-      <img src="~/assets/doddles/jobs-doodle.svg" class="w-52 absolute -top-40" />
+      <img src="~/assets/doddles/jobs-doodle.svg" class="w-28 md:w-52 absolute top-0 -right-16 md:right-auto md:-top-40" />
       <h2 class="text-4xl font-semibold mb-5 font-serif">Customers love Atlas</h2>
     </div>
-    <div class="md:flex gap-10 justify-between w-full pt-16">
-      <div class="md:w-1/3 h-72 rounded-3xl pt-14 pl-14 p-10 bg-gray-200 border border-gray-300 relative">
+    <div class="flex flex-row gap-10 justify-between w-full pt-16 pl-5 md:pl-0 overflow-x-auto">
+      <div class="flex-shrink-0 w-72 md:w-1/3 h-72 rounded-3xl pt-14 pl-14 p-10 bg-gray-200 border border-gray-300 relative">
         <p class="">
           I always tell them it’s the breadth of challenges, scale, engineering talent, big goals and the constant drive to achieve them.
         </p>
@@ -15,7 +15,7 @@
         <img src="~/assets/doddles/quote.svg" class="absolute -top-5 -left-5" />
       </div>
       
-      <div class="md:w-1/3 h-72 rounded-3xl pt-14 pl-14 p-10 bg-gray-200 border border-gray-300 relative">
+      <div class="flex-shrink-0 w-72 md:w-1/3 h-72 rounded-3xl pt-14 pl-14 p-10 bg-gray-200 border border-gray-300 relative">
         <p class="">
           I always tell them it’s the breadth of challenges, scale, engineering talent, big goals and the constant drive to achieve them.
         </p>
@@ -25,7 +25,7 @@
         <img src="~/assets/doddles/quote.svg" class="absolute -top-5 -left-5" />
       </div>
       
-      <div class="md:w-1/3 h-72 rounded-3xl pt-14 pl-14 p-10 bg-gray-200 border border-gray-300 relative">
+      <div class="flex-shrink-0 w-72 md:w-1/3 h-72 rounded-3xl pt-14 pl-14 p-10 bg-gray-200 border border-gray-300 relative">
         <p class="">
           I always tell them it’s the breadth of challenges, scale, engineering talent, big goals and the constant drive to achieve them.
         </p>
